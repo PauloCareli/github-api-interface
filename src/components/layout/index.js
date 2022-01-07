0e81a878c6d286a35af76b1024bcf2123cf5f9c4
PH <phpcareli@gmail.com>
2022-01-07 13:35:47 -0300

    update

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -3,12 +3,12 @@ import Header from "../header";
 import * as S from "./styled";
 
 const Layout = ({ children }) => {
-  return (
-    <S.WrapperLayout>
-      <Header />
-      {children}
-    </S.WrapperLayout>
-  );
+    return (
+        <S.WrapperLayout>
+            <Header />
+            {children}
+        </S.WrapperLayout>
+    );
 };
 
 export default Layout;
